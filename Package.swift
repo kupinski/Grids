d259ae4bb9577ba64e1b42fa4559f0946b995ecf
Matthew Kupinski <mkupinski@optics.arizona.edu>
2021-08-30 10:15:08 -0700

    Cleaned up the package manifest.

--- Package.swift
+++ Package.swift
@@ -5,20 +5,12 @@ import PackageDescription
 
 let package = Package(
     name: "Grids",
-    platforms: [.macOS(.v10_10), .iOS(.v8)],
     products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "Grids",
             targets: ["Grids"]),
     ],
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
-    ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "Grids",
             dependencies: []),
