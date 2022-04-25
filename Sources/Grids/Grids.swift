672d90425a25c69181fdc662fbe61638c670683c
Matthew Kupinski <kupinski@mac.com>
2022-04-25 09:57:53 -0700

    Added proper header.

--- Sources/Grids/Grids.swift
+++ Sources/Grids/Grids.swift
@@ -1,3 +1,26 @@
+/*
+   * [Ziteo, Inc] ("Ziteo") CONFIDENTIAL
+   * Unpublished Copyright (c) 2022 [Ziteo, Inc], All Rights Reserved.
+   *
+   * NOTICE:  All information contained herein is, and remains the property of Ziteo. The intellectual
+   * and technical concepts contained herein are proprietary to Ziteo and may be covered by U.S. and
+   * Foreign Patents, patents in process, and are protected by trade secret or copyright law.
+   * Dissemination of this information or reproduction of this material is strictly forbidden unless
+   * prior written permission is obtained from Ziteo. Access to the source code contained herein is
+   * hereby forbidden to anyone except current Ziteo employees, managers or contractors who have
+   * executed Confidentiality and Non-disclosure agreements explicitly covering such access.
+   *
+   * The copyright notice above does not evidence any actual or intended publication or disclosure of
+   * this source code, which includes information that is confidential and/or proprietary, and is a
+   * trade secret, of  Ziteo.   ANY REPRODUCTION, MODIFICATION, DISTRIBUTION, PUBLIC  PERFORMANCE, OR
+   * PUBLIC DISPLAY OF OR THROUGH USE  OF THIS  SOURCE CODE  WITHOUT  THE EXPRESS WRITTEN CONSENT OF
+   * Ziteo IS STRICTLY PROHIBITED, AND IN VIOLATION OF APPLICABLE LAWS AND INTERNATIONAL TREATIES. THE
+   * RECEIPT OR POSSESSION OF  THIS SOURCE CODE AND/OR RELATED INFORMATION DOES NOT CONVEY OR IMPLY
+   * ANY RIGHTS TO REPRODUCE, DISCLOSE OR DISTRIBUTE ITS CONTENTS, OR TO MANUFACTURE, USE, OR SELL
+   * ANYTHING THAT IT  MAY DESCRIBE, IN WHOLE OR IN PART.
+ */
+
+
 import Foundation
 
 /// A `LinearSpace` defines a 1-dimensional linear array which has a ``size``, a ``center``, and a number of pixels ``numPix``.  FIXME:  More here.
