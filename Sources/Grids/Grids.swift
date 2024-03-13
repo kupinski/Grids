f6f97475ed0be384e19f1fafd7adaffbaae63f61
kupinski <21088090+kupinski@users.noreply.github.com>
2024-03-13 12:02:39 -0700

    Changed comment block.  Ensure that Grids are Codable

--- Sources/Grids/Grids.swift
+++ Sources/Grids/Grids.swift
@@ -1,32 +1,30 @@
 /*
-   * [Ziteo, Inc] ("Ziteo") CONFIDENTIAL
-   * Unpublished Copyright (c) 2022 [Ziteo, Inc], All Rights Reserved.
-   *
-   * NOTICE:  All information contained herein is, and remains the property of Ziteo. The intellectual
-   * and technical concepts contained herein are proprietary to Ziteo and may be covered by U.S. and
-   * Foreign Patents, patents in process, and are protected by trade secret or copyright law.
-   * Dissemination of this information or reproduction of this material is strictly forbidden unless
-   * prior written permission is obtained from Ziteo. Access to the source code contained herein is
-   * hereby forbidden to anyone except current Ziteo employees, managers or contractors who have
-   * executed Confidentiality and Non-disclosure agreements explicitly covering such access.
-   *
-   * The copyright notice above does not evidence any actual or intended publication or disclosure of
-   * this source code, which includes information that is confidential and/or proprietary, and is a
-   * trade secret, of  Ziteo.   ANY REPRODUCTION, MODIFICATION, DISTRIBUTION, PUBLIC  PERFORMANCE, OR
-   * PUBLIC DISPLAY OF OR THROUGH USE  OF THIS  SOURCE CODE  WITHOUT  THE EXPRESS WRITTEN CONSENT OF
-   * Ziteo IS STRICTLY PROHIBITED, AND IN VIOLATION OF APPLICABLE LAWS AND INTERNATIONAL TREATIES. THE
-   * RECEIPT OR POSSESSION OF  THIS SOURCE CODE AND/OR RELATED INFORMATION DOES NOT CONVEY OR IMPLY
-   * ANY RIGHTS TO REPRODUCE, DISCLOSE OR DISTRIBUTE ITS CONTENTS, OR TO MANUFACTURE, USE, OR SELL
-   * ANYTHING THAT IT  MAY DESCRIBE, IN WHOLE OR IN PART.
+ * Copyright (c) 2023 University of Arizona [UofA], All Rights Reserved.
+ *
+ * NOTICE:  All information contained herein is, and remains the property of UofA. The intellectual
+ * and technical concepts contained herein are proprietary to UofA and may be covered by U.S. and
+ * Foreign Patents, patents in process, and are protected by trade secret or copyright law.
+ * Dissemination of this information or reproduction of this material is strictly forbidden unless
+ * prior written permission is obtained from UofA. Access to the source code contained herein is
+ * hereby forbidden to anyone except current UofA employees, managers or contractors who have
+ * executed Confidentiality and Non-disclosure agreements explicitly covering such access.
+ *
+ * The copyright notice above does not evidence any actual or intended publication or disclosure of
+ * this source code, which includes information that is confidential and/or proprietary, and is a
+ * trade secret, of  UofA.   ANY REPRODUCTION, MODIFICATION, DISTRIBUTION, PUBLIC  PERFORMANCE, OR
+ * PUBLIC DISPLAY OF OR THROUGH USE  OF THIS  SOURCE CODE  WITHOUT  THE EXPRESS WRITTEN CONSENT OF
+ * UofA IS STRICTLY PROHIBITED, AND IN VIOLATION OF APPLICABLE LAWS AND INTERNATIONAL TREATIES. THE
+ * RECEIPT OR POSSESSION OF  THIS SOURCE CODE AND/OR RELATED INFORMATION DOES NOT CONVEY OR IMPLY
+ * ANY RIGHTS TO REPRODUCE, DISCLOSE OR DISTRIBUTE ITS CONTENTS, OR TO MANUFACTURE, USE, OR SELL
+ * ANYTHING THAT IT  MAY DESCRIBE, IN WHOLE OR IN PART.
  */
-
 
 import Foundation
 
 /// A `LinearSpace` defines a 1-dimensional linear array which has a ``size``, a ``center``, and a number of pixels ``numPix``.  FIXME:  More here.
 ///
 /// You can define a `LinearSpace` either by its center, size, and number of pixels, or by it's start and end position.  Units are unspecified and up to the user to keep track of.
-public struct LinearSpace<T: BinaryFloatingPoint> {
+public struct LinearSpace<T: BinaryFloatingPoint & Codable> : Codable {
     /// The length of the linear space in unspecified units.
     public var size: T {
         didSet {
@@ -116,7 +114,7 @@ public struct LinearSpace<T: BinaryFloatingPoint> {
 /// Create a 2-D pixel grid.
 ///
 /// A 2-D pixel grid is defined by two separate ``LinearSpace``s.  One for the x-dimension and one for the y-dimension.  Units are undefined and left up to the user.
-public struct PixelGrid<T: BinaryFloatingPoint & SIMDScalar> {
+public struct PixelGrid<T: BinaryFloatingPoint & SIMDScalar & Codable> : Codable {
     /// The x-axis linear space
     public var xSpace: LinearSpace<T>
     /// The y-axis linear space
@@ -160,7 +158,7 @@ public struct PixelGrid<T: BinaryFloatingPoint & SIMDScalar> {
 /// A 3D voxel spaced defining  x-, y-, and z- ``LinearSpace``s.
 ///
 /// Units are up to the user and left unspecified in this structure.  Order of all `SIMD3` values are x, y, z.
-public struct VoxelGrid<T: BinaryFloatingPoint & SIMDScalar> {
+public struct VoxelGrid<T: BinaryFloatingPoint & SIMDScalar & Codable> : Codable {
     /// The x-axis ``LinearSpace``
     public var xSpace: LinearSpace<T>
     /// The y-axis ``LinearSpace``
